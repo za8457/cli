3a996de2832d16b6062230cddd2004432717b546
Vincent Bailly <vincent.bailly@microsoft.com>
2022-03-30 16:46:32 +0200

    finish testing shrinkwrap

--- workspaces/arborist/lib/arborist/reify.js
+++ workspaces/arborist/lib/arborist/reify.js
@@ -139,12 +139,10 @@ module.exports = cls => class Reifier extends cls {
   }
 
   async [_createIsolatedTree](idealTree) {
-    debugger
     await this[_makeIdealGraph](this.options)
 
     const proxiedIdealTree = this.idealGraph
 
-    debugger
     const hasher = (() => {
       const result = new Map()
       const idToLocation = new Map()
